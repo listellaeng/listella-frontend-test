522f59b756358a0111ecca1e2a3d20d013b40494
Jesse Goodenough <jesse.goodenough@gmail.com>
2023-02-01 21:18:35 -0500

    build out top left astronaut section of homepage

--- listella-engineering-team-skills-test/pages/index.tsx
+++ listella-engineering-team-skills-test/pages/index.tsx
@@ -1,9 +1,28 @@
 import Layout from '@/components/Layout';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <Layout>
-      <h1 className="mx-auto text-center">NASA Home page</h1>
+    <Layout title="NASA home page">
+      <div className={styles.HomePageLayout}>
+        <div className="h-1/2 p-4">
+          <div className="flex items-center justify-start py-4">
+            <p className="text-gray-500">Galleries &bull;</p>
+            <p className="font-semibold">Space Database</p>
+          </div>
+
+          <Image
+            src="/assets/astronaut.jpg"
+            alt="astronaut"
+            width={400}
+            height={600}
+            className="w-full h-auto rounded-xl"
+          />
+        </div>
+        <div className="h-1/2 bg-slate-50"></div>
+        <div className="h-1/2 bg-orange-50"></div>
+      </div>
     </Layout>
   );
 };
